2d5e3bc195dfac276a46235202272ce4ba1ad4d0
SebastianTorres <svelasco13@ucol.mx>
2023-10-04 12:10:59 -0600

    Primera version

---
+++ index.js
@@ -0,0 +1,25 @@
+/* let numero = 5;
+numero = 8;
+const constante = 4;
+/
+*/
+//let
+//const
+/*
+alert("Hola desde una alerta de Javascript");
+console.log("Esto es Javascript desde el HTML");
+console.log(numero);
+console.log(constante);
+*/
+//concatenacion
+
+let nombre = "Sebastian";
+let apellido = "Torres";
+console.log("Mi nombre es: "+nombre+" "+apellido);
+
+document.write("Hola en el documento desde JS")
+
+// let seleccion = document.querySelector("#seccion1");
+
+console.log(seleccion);
+
